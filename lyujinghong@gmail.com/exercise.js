5a122320554a3c562f7cc3767c1ed5787bb748d6
lyujinghong@gmail.com <lyujinghong@gmail.com>
2018-02-20 11:50:53 -0500

    Updated exercise file(s)

--- lyujinghong@gmail.com/exercise.js
+++ lyujinghong@gmail.com/exercise.js
@@ -15,9 +15,19 @@ exercise.findBiggerSalaries = function(){
     // ---------------------------------------------------
     //   YOUR CODE
     //
+    var dataLength = boston.data.length;
+    var salarylist = [];
+    var biggerlist = []; // a list that holds all salaries larger than certain amount
+    for (var i = 0; i < dataLength; i++) {
     //     Return a filtered list of salaries.
+        salarylist[i] = exercise.salaries[i][18];
     //     Return salaries larger than exercise.salary
-    //
+    //     Append the list
+        if (salarylist[i]> exercise.salary){
+            biggerlist.push(salarylist[i]);
+        }
+    }
+    return biggerlist;
     //     Example: salaries larger than 300K is 8
     // ---------------------------------------------------
 
@@ -29,6 +39,18 @@ exercise.largerSalary = function largerSalary(item){
     // ---------------------------------------------------
     //   YOUR CODE
     //
+    var dataLength = boston.data.length;
+    var salary = [];
+    var booleanlist = [];
+    for (var i = 0; i < dataLength; i++){
+        salarylist[i] = exercise.salaries[i][18];
+        if (salarylist[i] > exercise.salary){
+            booleanlist[i] = 'TRUE';
+        }
+        else{
+            booleanlist[i] = 'FALSE';
+        }
+    }
     //     Return a boolean (true/false) if item
     //     is larger than exercise.salary
     // ---------------------------------------------------
